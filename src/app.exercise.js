9b8aa708e32ae2068e298f9b4c9a2d3697e957c7
Ian MacIntosh <ian@ianjmacintosh.com>
2024-08-13 18:13:03 +0000

    Solve Ex4.01

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -2,36 +2,43 @@
 import {jsx} from '@emotion/core'
 
 import * as React from 'react'
-// 🐨 you're going to need this:
 import * as auth from 'auth-provider'
+import {client} from 'utils/api-client.exercise'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
 
 function App() {
-  // 🐨 useState for the user
-  const [user, setUser] = React.useState()
+  const [user, setUser] = React.useState(null)
 
-  // 🐨 create a login function that calls auth.login then sets the user
   const login = form => auth.login(form).then(u => setUser(u))
-
-  // 🐨 create a registration function that does the same as login except for register
   const register = form => auth.register(form).then(u => setUser(u))
-
-  // 🐨 create a logout function that calls auth.logout() and sets the user to null
-  const logout = () => auth.logout().then(() => setUser(null))
-
-  // 🐨 if there's a user, then render the AuthenticatedApp with the user and logout
-  if (user) {
-    return <AuthenticatedApp user={user} logout={logout} />
+  const logout = () => {
+    auth.logout()
+    setUser(null)
   }
 
-  // 🐨 if there's not a user, then render the UnauthenticatedApp with login and register
-  return <UnauthenticatedApp login={login} register={register} />
+  React.useEffect(() => {
+    async function reauth() {
+      console.log('Hellooo')
+      const token = await auth.getToken()
+      if (token) {
+        // we're logged in! Let's go get the user's data:
+        client('me', {token}).then(data => {
+          setUser(data.user)
+        })
+      } else {
+        // we're not logged in. Show the login screen
+      }
+    }
+
+    reauth()
+  }, [])
+
+  return user ? (
+    <AuthenticatedApp user={user} logout={logout} />
+  ) : (
+    <UnauthenticatedApp login={login} register={register} />
+  )
 }
 
 export {App}
-
-/*
-eslint
-  no-unused-vars: "off",
-*/
