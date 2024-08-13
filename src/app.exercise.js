0fd4753c9b535a1497e1c653f0bde573cdc7b824
Ian MacIntosh <ian@ianjmacintosh.com>
2024-08-13 18:32:40 +0000

    Solve Ex4.02

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -3,12 +3,40 @@ import {jsx} from '@emotion/core'
 
 import * as React from 'react'
 import * as auth from 'auth-provider'
-import {client} from 'utils/api-client.exercise'
+import {client} from './utils/api-client'
+import {useAsync} from 'utils/hooks'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
+import {FullPageSpinner} from 'components/lib'
+import * as colors from './styles/colors'
+
+async function getUser() {
+  let user = null
+
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+
+  return user
+}
 
 function App() {
-  const [user, setUser] = React.useState(null)
+  const {
+    data: user,
+    error,
+    isIdle,
+    isLoading,
+    isSuccess,
+    isError,
+    run,
+    setData: setUser,
+  } = useAsync()
+
+  React.useEffect(() => {
+    getUser().then(u => setUser(u))
+  }, [])
 
   const login = form => auth.login(form).then(u => setUser(u))
   const register = form => auth.register(form).then(u => setUser(u))
@@ -17,28 +45,31 @@ function App() {
     setUser(null)
   }
 
-  React.useEffect(() => {
-    async function reauth() {
-      console.log('Hellooo')
-      const token = await auth.getToken()
-      if (token) {
-        // we're logged in! Let's go get the user's data:
-        client('me', {token}).then(data => {
-          setUser(data.user)
-        })
-      } else {
-        // we're not logged in. Show the login screen
-      }
-    }
-
-    reauth()
-  }, [])
-
-  return user ? (
-    <AuthenticatedApp user={user} logout={logout} />
-  ) : (
-    <UnauthenticatedApp login={login} register={register} />
-  )
+  if (isLoading || isIdle) {
+    return <FullPageSpinner></FullPageSpinner>
+  } else if (isError) {
+    return (
+      <div
+        css={{
+          color: colors.danger,
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <p>Uh oh... There's a problem. Try refreshing the app.</p>
+        <pre>{error.message}</pre>
+      </div>
+    )
+  } else {
+    return user ? (
+      <AuthenticatedApp user={user} logout={logout} />
+    ) : (
+      <UnauthenticatedApp login={login} register={register} />
+    )
+  }
 }
 
 export {App}
