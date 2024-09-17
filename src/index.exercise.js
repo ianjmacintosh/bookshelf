f5a5d76a5b376c7e5161dc643be0735a72f7bccc
Ian MacIntosh <ian@ianjmacintosh.com>
2024-09-17 17:42:32 +0000

    Solve Ex6.02

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -3,12 +3,34 @@ import './bootstrap'
 import * as React from 'react'
 import {createRoot} from 'react-dom/client'
 import {App} from './app'
+import {ReactQueryConfigProvider} from 'react-query/dist/react-query'
 
 // ignore the rootRef in this file. I'm just doing it here to make
 // the tests I write to check your work easier.
 export const rootRef = {}
+
+const queryConfig = {
+  queries: {
+    useErrorBoundary: true,
+    refetchOnWindowFocus: false,
+    retry: (failureCount, error) => {
+      console.log(typeof error.status)
+      if (error?.status === 404 || failureCount > 2) {
+        return false
+      }
+      return true
+    },
+  },
+  mutations: {
+    refetchOnWindowFocus: true,
+  },
+}
 loadDevTools(() => {
   const root = createRoot(document.getElementById('root'))
-  root.render(<App />)
+  root.render(
+    <ReactQueryConfigProvider config={queryConfig}>
+      <App />
+    </ReactQueryConfigProvider>,
+  )
   rootRef.current = root
 })
