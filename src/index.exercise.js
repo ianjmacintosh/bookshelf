798447e9fdc972b997bebe76cecaeaf01732a353
Ian MacIntosh <ian@ianjmacintosh.com>
2024-09-17 17:51:28 +0000

    Prepare for Ex6.03

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -2,29 +2,24 @@ import {loadDevTools} from './dev-tools/load'
 import './bootstrap'
 import * as React from 'react'
 import {createRoot} from 'react-dom/client'
+import {ReactQueryConfigProvider} from 'react-query'
 import {App} from './app'
-import {ReactQueryConfigProvider} from 'react-query/dist/react-query'
-
-// ignore the rootRef in this file. I'm just doing it here to make
-// the tests I write to check your work easier.
-export const rootRef = {}
 
 const queryConfig = {
   queries: {
     useErrorBoundary: true,
     refetchOnWindowFocus: false,
-    retry: (failureCount, error) => {
-      console.log(typeof error.status)
-      if (error?.status === 404 || failureCount > 2) {
-        return false
-      }
-      return true
+    retry(failureCount, error) {
+      if (error.status === 404) return false
+      else if (failureCount < 2) return true
+      else return false
     },
   },
-  mutations: {
-    refetchOnWindowFocus: true,
-  },
 }
+
+// ignore the rootRef in this file. I'm just doing it here to make
+// the tests I write to check your work easier.
+export const rootRef = {}
 loadDevTools(() => {
   const root = createRoot(document.getElementById('root'))
   root.render(
