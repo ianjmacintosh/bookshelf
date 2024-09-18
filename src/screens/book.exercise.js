9012294ead7f777c45b50808616b64e08b66d23a
Ian MacIntosh <ian@ianjmacintosh.com>
2024-09-18 19:03:56 +0000

    Setup for Ex6.04

--- src/screens/book.exercise.js
+++ src/screens/book.exercise.js
@@ -11,7 +11,7 @@ import {useListItem, useUpdateListItem} from 'utils/list-items'
 import {formatDate} from 'utils/misc'
 import * as mq from 'styles/media-queries'
 import * as colors from 'styles/colors'
-import {ErrorMessage, Textarea} from 'components/lib'
+import {Textarea, ErrorMessage} from 'components/lib'
 import {Rating} from 'components/rating'
 import {StatusButtons} from 'components/status-buttons'
 
@@ -103,10 +103,9 @@ function ListItemTimeframe({listItem}) {
 
 function NotesTextarea({listItem, user}) {
   const [mutate, {error, isError}] = useUpdateListItem(user)
-  const debouncedMutate = React.useMemo(
-    () => debounceFn(mutate, {wait: 300}),
-    [mutate],
-  )
+  const debouncedMutate = React.useMemo(() => debounceFn(mutate, {wait: 300}), [
+    mutate,
+  ])
 
   function handleNotesChange(e) {
     debouncedMutate({id: listItem.id, notes: e.target.value})
