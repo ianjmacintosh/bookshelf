c3bdb86b053ad52899f6febd7f6191768769b75b
Ian MacIntosh <ian@ianjmacintosh.com>
2024-09-19 20:06:39 +0000

    Create context and wrap app in it

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -1,2 +1,8 @@
 // 🐨 create and export a React context variable for the AuthContext
 // 💰 using React.createContext
+
+import {createContext} from 'react'
+
+const AuthContext = createContext(null)
+
+export {AuthContext}
